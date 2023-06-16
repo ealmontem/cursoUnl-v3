e861f70e4ba1f9cee44103bbdac0a9ffbb94bd7b
ealmonte <elvisalmonte14@gmail.com>
2023-06-16 18:54:47 -0400

    first commit

---
+++ src/main/java/com/unl/cursounl/CursoUnlApplication.java
@@ -0,0 +1,13 @@
+package com.unl.cursounl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUnlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoUnlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unl/cursounl/CursoUnlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unl.cursounl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUnlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
